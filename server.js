1051fc692fe9311a3b4dc571a819548eb5355636
Pedro Alvarado <pedro.alvarado@turner.com>
2019-07-05 16:26:53 -0500

    conecting to MongoDB with Mongoose

--- server.js
+++ server.js
@@ -1,5 +1,10 @@
 const express = require('express')
+const connectDB = require('./config/db')
+
 const app = express()
+
+// connect database
+connectDB();
 
 app.get('/', (req, res) => res.send('API running...'))
 const PORT = process.env.PORT || 5000
