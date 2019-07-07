642401ab8fe4069f9f66f04186f09f3412740d2e
Pedro Alvarado <pedro.alvarado@turner.com>
2019-07-07 04:23:53 -0500

    add endpoint for getting current profile

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,9 +1,28 @@
 const express = require("express");
 const router = express.Router();
+const auth = require("../../middleware/auth");
+const Profile = require("../../models/profile");
+const User = require("../../models/user");
 
-// @route         GET api/profile
-// @description   Test route
+// @route         GET api/profile/me
+// @description   Get profile for current user
 // @access        Publinc
-router.get("/", (req, res) => res.send("Profile route"));
+router.get("/me", auth, async (req, res) => {
+  try {
+    const profile = await Profile.findOne({ user: req.user.id }).populate(
+      "user",
+      ["name", "avatar"]
+    );
+
+    if (!profile) {
+      return res.status(400).json({ msg: "There is no profile for this user" });
+    }
+
+    return res.json(profile);
+  } catch (err) {
+    console.log(err.message);
+    res.status(500).send("Server error");
+  }
+});
 
 module.exports = router;
