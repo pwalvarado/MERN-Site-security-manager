d923a2277d40e68bd7e886a26c7c333516b537a8
Pedro Alvarado <pedro.alvarado@turner.com>
2019-07-11 15:33:38 -0500

    simple request without redux, please rollback

--- client/src/components/auth/Signup.js
+++ client/src/components/auth/Signup.js
@@ -1,4 +1,5 @@
 import React, { Fragment, useState } from "react";
+import axios from "axios";
 
 const Signup = () => {
   const [formData, setFormData] = useState({
@@ -13,12 +14,30 @@ const Signup = () => {
   const onChange = e =>
     setFormData({ ...formData, [e.target.name]: e.target.value });
 
-  const onSubmit = e => {
+  const onSubmit = async e => {
     e.preventDefault();
     if (password !== password2) {
       console.log("Passwords don't match");
     } else {
-      console.log(formData);
+      const newUser = {
+        name,
+        email,
+        password
+      };
+
+      try {
+        const config = {
+          headers: {
+            "Content-Type": "application/json"
+          }
+        };
+
+        const body = JSON.stringify(newUser);
+        const res = await axios.post("/api/users", body, config);
+        console.log(res.data);
+      } catch (error) {
+        console.error(error.response.data);
+      }
     }
   };
 
