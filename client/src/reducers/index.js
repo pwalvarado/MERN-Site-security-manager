0dbc397e90705d322b7c0586fad4c8ceaf962573
Pedro Alvarado <pedro.alvarado@turner.com>
2019-07-14 16:16:10 -0500

    add profile reducer, dashboar component, and get current pofile

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from "redux";
 import alertReducer from "./alert";
 import auth from "./auth";
+import profile from "./profile";
 
 export default combineReducers({
   alert: alertReducer,
-  auth
+  auth,
+  profile
 });
